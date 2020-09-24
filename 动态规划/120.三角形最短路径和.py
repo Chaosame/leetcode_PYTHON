0e143ea40a24ecd4124a0a3455d3d9306cb9eb51
Chaos <448473621@qq.com>
2020-09-24 21:48:26 +0800

    更新优化方法
    120.  优化了思维和算法

--- 动态规划/120.三角形最短路径和.py
+++ 动态规划/120.三角形最短路径和.py
@@ -7,6 +7,7 @@ class Solution:
         #   这里有 序号i只能到下一行的序号i或者i+1
         #   存在短视情况，如何处理？相当于只能往下或者往右下走
         #   递推：dp[i][j] = min(dp[i-1][j]+dp[i-1][j]) + triangle[i+1][j+1] i>1
+
         if len(triangle) == 0:
             return 0
         if len(triangle) == 1:
@@ -29,7 +30,17 @@ class Solution:
 
         return min_path
 
+    """
+        这题最恶心的在于越界问题，非常容易写着写着就越界了，此外如果从上往下，约束条件非常地不好写，
+        此外获得最后的结果在从上往下的算法中也需要再次遍历最后一行获得，这非常的不方便
+        优化方法：自底向上，把约束简化。除此之外我们可以直接利用triangle本身而不用新建dp数组，同时因为顶点是唯一的，所以只要输出[0][0]就行了
+        递推：dp[i][j]= min(dp[i+1][j],dp[i+1][j+1])+dp[i][j]
+        边界条件：最后一行没有下一行了，因此最后一行不进循环，循环到第1行为止
+        
+    """
 
-"""
-    优化方法：自底向上，把约束简化
-"""
+    def minpath2(self, triangle: List[List[int]]) -> int:
+        for i in range(len(triangle)-2,-1,-1):
+            for j in range (len(triangle[i])):
+                triangle[i][j]= triangle[i][j]+min(triangle[i+1][j],triangle[i+1][j+1])
+        return triangle[0][0]
